dc48fe45fb8b393fdc850687b8371147fa8d1437
DucTuan <tuanhypc02@gmail.com>
2023-12-28 15:27:54 +0700

    tao class

---
+++ simpsonBook/detailsVC.swift
@@ -0,0 +1,27 @@
+//
+//  detailsVC.swift
+//  simpsonBook
+//
+//  Created by macOS on 28/12/2023.
+//
+
+import UIKit
+
+class detailsVC: UIViewController {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+        
+    }
+    
+
+   
+
+}
---
+++ simpsonBook/simpson.swift
@@ -0,0 +1,16 @@
+//
+//  simpson.swift
+//  simpsonBook
+//
+//  Created by macOS on 28/12/2023.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    var name = ""
+    var job = ""
+    var image = UIImage()
+    
+}
